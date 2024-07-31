190e4d7d6d1e027cfcab40e14359893257b5e79d
Jawid <jawidgulzai9@gmail.com>
2024-07-31 13:47:11 -0700

    Some changes

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,29 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+
+    private static WebDriver driver;
+
+    public void setupBrowser(){
+        driver = new ChromeDriver();
+        driver.get("https://tek-retail-ui.azurewebsites.net/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+    }
+
+    public void quitBrowser(){
+        if (driver != null){
+            driver.quit();
+
+        }
+    }
+    public WebDriver getDriver(){
+        return driver;
+    }
+}
---
+++ src/test/java/tek/bdd/steps/SetupTestSteps.java
@@ -0,0 +1,29 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import tek.bdd.utility.SeleniumUtility;
+
+public class SetupTestSteps extends SeleniumUtility {
+
+    @Given("Open browser and navigate to retail app")
+    public void openBrowserAndNavigateToRetailApp() {
+        setupBrowser();
+
+    }
+
+    @Then("Validate top left corner is TEKSCHOOL")
+    public void validateTopLeftCornerIsTekSchool() {
+        String topLeftCornerTekSchoolLogo = getElementText(By.linkText("TEKSCHOOL"));
+        Assert.assertEquals("TEKSCHOOL", topLeftCornerTekSchoolLogo);
+        System.out.println(topLeftCornerTekSchoolLogo);
+    }
+
+    @Then("Close browser")
+    public void closeBrowser() {
+        quitBrowser();
+
+    }
+}
